85e7b1cb8ba74d93b31060148f1301cfef2cbbb0
David-396 <david.eini12@gmail.com>
2025-08-27 14:31:46 +0300

    run the process manager

--- app/preprocessor/main.py
+++ app/preprocessor/main.py
@@ -1,6 +1,8 @@
+import time
 from app.preprocessor.manager import PreProcessorManager
 
 if __name__ == '__main__':
+
     manager = PreProcessorManager()
 
     while True:
@@ -11,3 +13,5 @@ if __name__ == '__main__':
         manager.consume_process_produce(consume_topic='raw_tweets_not_antisemitic',
                                      produce_topic='preprocessed_tweets_not_antisemitic',
                                      group_id='group-1')
+
+        time.sleep(0.5)
