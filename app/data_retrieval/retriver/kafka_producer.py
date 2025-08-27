612cfe2d329e0a5b10d1eb7218a40afa5ff0b051
Elazar <e0548448478@gmail.com>
2025-08-27 11:51:46 +0300

    db client to connet to database added

---
+++ app/data_retrieval/retriver/kafka_producer.py
@@ -0,0 +1,56 @@
+import os
+import json
+import asyncio
+import logging
+from aiokafka import AIOKafkaProducer
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+KAFKA_BROKER_URL = os.getenv("KAFKA_BROKER_URL", "localhost:9092")
+
+class KafkaProducer:
+    def __init__(self):
+        self.producer = None
+
+    async def start_producer(self):
+        self.producer = AIOKafkaProducer(
+            bootstrap_servers=KAFKA_BROKER_URL,
+            value_serializer=lambda v: json.dumps(v).encode("utf-8")
+        )
+        await self.producer.start()
+        logger.info("Kafka producer started")
+
+    async def stop_producer(self):
+        if self.producer:
+            await self.producer.stop()
+            logger.info("Kafka producer stopped")
+
+    async def send_message(self,topic: str, message: dict):
+        if self.producer is None:
+            raise RuntimeError("Producer not initialized")
+        await self.producer.send_and_wait(topic, message)
+        logger.info(f"Sent message to {topic}: {str(message)[:30]}...")
+
+
+# producer = None
+#
+# async def start_producer():
+#     global producer
+#     producer = AIOKafkaProducer(
+#         bootstrap_servers=KAFKA_BROKER_URL,
+#         value_serializer=lambda v: json.dumps(v).encode("utf-8")
+#     )
+#     await producer.start()
+#     logger.info("Kafka producer started")
+#
+# async def stop_producer():
+#     if producer:
+#         await producer.stop()
+#         logger.info("Kafka producer stopped")
+#
+# async def send_message(topic: str, message: dict):
+#     if producer is None:
+#         raise RuntimeError("Producer not initialized")
+#     await producer.send_and_wait(topic, message)
+#     logger.info(f"Sent message to {topic}: {str(message)[:30]}...")
