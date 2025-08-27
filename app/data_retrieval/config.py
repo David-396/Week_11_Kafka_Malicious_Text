7e5e82e10896b525aadc7bca61fa4941374b821a
Elazar <e0548448478@gmail.com>
2025-08-27 17:40:14 +0300

    the retriever finished

---
+++ app/data_retrieval/config.py
@@ -0,0 +1,14 @@
+import os
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+MDB_USER = os.getenv("MDB_USER")
+MDB_PASSWORD = os.getenv("MDB_PASSWORD")
+MDB_HOST = os.getenv("MDB_HOST")
+DB_NAME = os.getenv("DB_NAME", "IranMalDB")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME", "tweets")
+MDB_URI = f"mongodb+srv://{MDB_USER}:{MDB_PASSWORD}@{MDB_HOST}/"
+
