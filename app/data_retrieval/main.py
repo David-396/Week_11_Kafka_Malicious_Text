98eea8582fbf5b3abba156cec4b6850cdbaf47ac
David-396 <david.eini12@gmail.com>
2025-09-02 10:19:06 +0300

    fix fastapi

--- app/data_retrieval/main.py
+++ app/data_retrieval/main.py
@@ -1,29 +1,36 @@
 import os
 import pymongo
+import uvicorn
 from fastapi import FastAPI
+from starlette.responses import JSONResponse
 
 app = FastAPI()
 
 
 
 
-MONGO_HOST = os.environ.get('MONGO_HOST', 'mongo_db')
+MONGO_HOST = os.environ.get('MONGO_HOST', 'localhost')
 MONGO_PORT = os.environ.get('MONGO_PORT', 27017)
 DB_NAME = os.environ.get('DB_NAME', 'tweets')
 ANTISEMITE_COLLECTION_NAME = os.environ.get('ANTISEMITE_DB_NAME', 'tweets_antisemitic')
 NOT_ANTISEMITE_COLLECTION_NAME = os.environ.get('NOT_ANTISEMITE_COLLECTION_NAME', 'tweets_not_antisemitic')
 
+MDB_USER = os.getenv("MDB_USER", "root")
+MDB_PASSWORD = os.getenv("MDB_PASSWORD", "example")
+
+MDB_URI = f"mongodb://{MDB_USER}:{MDB_PASSWORD}@{MONGO_HOST}/{DB_NAME}?authSource=admin"
+
 @app.get('/anti')
-def get_antisemietic_tweets():
+def get_antisemitic_tweets():
     try:
 
-        client = pymongo.MongoClient(f"mongodb://{MONGO_HOST}:{MONGO_PORT}/")
+        client = pymongo.MongoClient(MDB_URI)
         mydb = client[DB_NAME]
         collection = mydb[ANTISEMITE_COLLECTION_NAME]
 
-        data = collection.find()
+        data = collection.find({},{'_id':0}).to_list()
 
-        return data
+        return JSONResponse(content=data)
 
     except Exception as e:
         print(e)
@@ -35,17 +42,20 @@ def get_antisemietic_tweets():
 
 
 @app.get('/not-anti')
-def get_not_antisemietic_tweets():
+def get_not_antisemitic_tweets():
     try:
 
-        client = pymongo.MongoClient(f"mongodb://{MONGO_HOST}:{MONGO_PORT}/")
+        client = pymongo.MongoClient(MDB_URI)
         mydb = client[DB_NAME]
         collection = mydb[NOT_ANTISEMITE_COLLECTION_NAME]
 
-        data = collection.find()
+        data = collection.find({},{'_id':0}).to_list()
 
-        return data
+        return JSONResponse(content=data)
 
     except Exception as e:
         print(e)
         return e
+
+
+uvicorn.run(app=app, host='localhost', port=8080)
