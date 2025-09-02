201004f54bb8aa83b2a386239a7e491afd2d76ca
David-396 <david.eini12@gmail.com>
2025-09-02 10:20:38 +0300

    run mongo script

--- scripts/scripts.bat
+++ scripts/scripts.bat
@@ -1,3 +1,7 @@
+ docker run -d -p 27017:27017 --name mongo_project -e MONGO_INITDB_ROOT_USERNAME=root -e MONGO_INITDB_ROOT_PASSWORD=example mongo:6.0
+
+
+
 oc apply -f ..\infra\retriever_infra\secret.yaml
 oc apply -f ..\infra\retriever_infra\deployment.yaml
 oc apply -f ..\infra\retriever_infra\svc.yaml
