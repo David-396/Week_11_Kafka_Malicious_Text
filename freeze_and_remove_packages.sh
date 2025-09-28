859b632725992873b5e1d2f1d5f59741d2920ca3
David-396 <david.eini12@gmail.com>
2025-09-28 14:06:38 +0300

    adding remove packages file

--- freeze_and_remove_packages.sh
+++ freeze_and_remove_packages.sh
@@ -2,3 +2,6 @@
 python -m pip install --upgrade --force-reinstall pip
 pip freeze > requirements.txt
 pip uninstall -r requirements.txt -y
+git add .
+git commit -m "adding remove packages file"
+git push
